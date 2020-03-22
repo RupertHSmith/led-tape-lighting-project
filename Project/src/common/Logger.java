64bdaf2c17ad46d9e6d63462d943a9f3af2d73bf
rhs1g18 <rhs1g18@soton.ac.uk>
2020-03-22 20:34:20 +0000

    Implemented logger class

--- Project/src/common/Logger.java
+++ Project/src/common/Logger.java
@@ -1,18 +1,52 @@
 package common;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Logger {
-    public Logger(){
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy-HH:mm:ss");
-        LocalDateTime now = LocalDateTime.now();
-        String currentDateTime = dtf.format(now);
+    private String fileName;
+
+    public Logger() throws IOException {
+        String currentDateTime = getCurrentTime();
 
         String fileName = "java-log-" + currentDateTime + ".log";
         File logFile = new File(fileName);
+        if(logFile.createNewFile()){
+            System.out.println("Log file created");
+        } else {
+            throw new IOException("File exists or could not be created");
+        }
+    }
 
+    private String getCurrentTime(){
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy-HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        return dtf.format(now);
+    }
 
+    public synchronized void writeMessage(Object fromClass, String errorMessage) throws IOException {
+        String tag =  "[" + getCurrentTime() + "] Message from class '" + fromClass.getClass().getSimpleName() + "':\n";
+        String message = tag + errorMessage + "\n";
+
+        if (!fileName.equals("")){
+            FileWriter fileWriter = new FileWriter(fileName);
+            fileWriter.write(message);
+            fileWriter.close();
+        }
+    }
+
+    public synchronized void writeError(Object fromClass, String errorMessage) throws IOException {
+        String tag =  "[" + getCurrentTime() + "] Error message from class '" + fromClass.getClass().getSimpleName() + "':\n";
+        String message = tag + errorMessage + "\n";
+
+        if (!fileName.equals("")){
+            FileWriter fileWriter = new FileWriter(fileName);
+            fileWriter.write(message);
+            fileWriter.close();
+        }
     }
 }
