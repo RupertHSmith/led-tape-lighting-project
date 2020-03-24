ee22167bcd0dca839556118bef7b220511234d31
rhs1g18 <rhs1g18@soton.ac.uk>
2020-03-24 14:02:04 +0000

    Added JavaDoc comments

--- Project/src/effects/TcpDirectFinishedListener.java
+++ Project/src/effects/TcpDirectFinishedListener.java
@@ -1,5 +1,9 @@
 package effects;
 
+/**
+ * Interface provides call back method that is called when
+ * TCP direct communication finishes
+ */
 public interface TcpDirectFinishedListener {
     public void tcpDirectFinished();
 }
