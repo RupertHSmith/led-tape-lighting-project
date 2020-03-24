17342d3460e388a44ad837872e92f0b7e9d61d37
rhs1g18 <rhs1g18@soton.ac.uk>
2020-03-24 14:07:57 +0000

    Added JavaDoc comments

--- Project/src/database/DatabaseListener.java
+++ Project/src/database/DatabaseListener.java
@@ -6,7 +6,20 @@ import effects.CustomEffect;
 
 import java.util.ArrayList;
 
+/**
+ * Implemented by any class that listens to device state
+ * and alarm state updates
+ */
 public interface DatabaseListener {
+    /**
+     * Callback method for when a device state is received
+     * @param deviceState The device state received during this update
+     */
     void onDeviceStateReceived(DeviceState deviceState);
+
+    /**
+     * Callback method for when an alarm state update is received
+     * @param alarms The list of alarms sent by the database
+     */
     void onAlarmStateReceived(ArrayList<Alarm> alarms);
 }
