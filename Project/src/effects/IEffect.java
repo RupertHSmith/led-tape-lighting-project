ca4e74994c4bc7ff5db5248a58ef3d7c1b3c953c
rhs1g18 <rhs1g18@soton.ac.uk>
2020-03-24 14:06:00 +0000

    Added JavaDoc comments

--- Project/src/effects/IEffect.java
+++ Project/src/effects/IEffect.java
@@ -2,7 +2,20 @@ package effects;
 
 import common.*;
 
+/**
+ * Interface implemented by any effect that can run on the device
+ * providing compulsory methods
+ */
 public interface IEffect {
-    public void start() throws TapeInUseException;
-    public LedState terminate();
+    /**
+     * Called to set the effect running,
+     * @throws TapeInUseException Thrown if the tape is currently being used by another effect and therefore cannot begin
+     */
+    void start() throws TapeInUseException;
+
+    /**
+     * Called to stop the effect running and release control of the Tape
+     * @return Returns the state of the tape after termination (i.e. most effects will halt as they were)
+     */
+    LedState terminate();
 }
