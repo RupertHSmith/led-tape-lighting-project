fb03f30b0f6e3b7c1066ccb8cc99146a709526cb
rhs1g18 <rhs1g18@soton.ac.uk>
2020-04-15 16:14:20 +0100

    Converted TCP direct to be UDP for more responsive control and added TAP function

--- RGBTapeControllerApp/app/src/main/java/com/rgbtape/app/TcpDirectFragment.java
+++ RGBTapeControllerApp/app/src/main/java/com/rgbtape/app/TcpDirectFragment.java
@@ -12,17 +12,33 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
+import android.widget.Toast;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.util.Collection;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static android.content.ContentValues.TAG;
 
@@ -30,7 +46,31 @@ public class TcpDirectFragment extends Fragment {
     private BlockingQueue<TcpPacketContainer> packetQueue = new LinkedBlockingQueue<>();
     private boolean tcpConnectionInProgres = false;
     private boolean buttonLocked = false;
-    private ServerSocket ss;
+    private InetAddress deviceIp;
+    private DatagramSocket socket;
+    private BpmCalculator bpmCalculator;
+
+    private ExecutorService executorService;
+
+
+    //Packets will remove
+    private DatagramPacket customBrighnessPacket;
+    private DatagramPacket onWhite;
+    private DatagramPacket onBlue;
+    private DatagramPacket offFade;
+    private DatagramPacket off;
+    byte[] customBrightness;
+
+    private Timer timer;
+    private Timer scheduleTimer;
+    public static long RESET_DURATION = 2000;
+
+    private static final String IF_CONFIG = "ifconfig wlan0";
+    private static final String DEFAULT_BROADCAST_ADDRESS = "192.168.1.255";
+    private static final int SOCKET_TIME_OUT = 500;
+    private static final byte UDP_VERSION = 1;
+    private static final int UDP_INIT_PORT = 5558;
+    private static final int UDP_PORT = 5557;
 
     @Nullable
     @Override
@@ -41,117 +81,132 @@ public class TcpDirectFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        executorService = Executors.newSingleThreadExecutor();
 
         View view = getView();
-        if (view != null) {
-            initTcpConnectButton(view);
+        if (view != null){
+            bpmCalculator = new BpmCalculator();
+            initConnectDisconnectButtons(view);
             initShutdownRebootButtons(view);
+            initFlashButtons(view);
         }
     }
 
-    private synchronized void setTcpConnectionInProgres(boolean tcpConnectionInProgres){
-        this.tcpConnectionInProgres = tcpConnectionInProgres;
+    private void establishDeviceIp(){
+        try {
+
+            String broadCastAddressString = getBroadcastAddressLinux();
+            InetAddress broadcastAddress;
+
+            if (broadCastAddressString != null) {
+                broadcastAddress = InetAddress.getByName(broadCastAddressString);
+            } else {
+                String message = "Using default broadcast address: 192.168.1.255";
+                broadcastAddress = InetAddress.getByName(DEFAULT_BROADCAST_ADDRESS);
+            }
+
+            if (socket == null) {
+                socket = new DatagramSocket(UDP_PORT);
+                socket.setSoTimeout(SOCKET_TIME_OUT);
+            }
+
+            byte[] buf = "rupertrgbtape".getBytes();
+            DatagramPacket packet = new DatagramPacket(buf, buf.length, broadcastAddress, UDP_INIT_PORT);
+            socket.send(packet);
+
+            buf = new byte[1];
+            packet = new DatagramPacket(buf, buf.length);
+            socket.receive(packet);
+
+            if (packet.getData()[0] == UDP_VERSION) {
+                deviceIp = packet.getAddress();
+                initialisePackets();
+            }
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
-    private synchronized boolean isTcpConnectionInProgres(){
-        return this.tcpConnectionInProgres;
+    private void closeConnection(){
+        byte[] closeConnection = new byte[8];
+        closeConnection[0] = 100;
+        try {
+            if (deviceIp != null && socket != null)
+                socket.send(new DatagramPacket(closeConnection, closeConnection.length, deviceIp, UDP_PORT));
+        } catch (IOException e){
+            e.printStackTrace();
+        }
     }
 
-    private synchronized void setButtonLocked(boolean buttonLocked){
-        this.buttonLocked = buttonLocked;
-    }
-
-    private synchronized boolean isButtonLocked(){
-        return this.buttonLocked;
-    }
-
-    private void initTcpConnectButton(View view){
-        final Switch tcpConnectSwitch = view.findViewById(R.id.tcpConnectionButton);
-        tcpConnectSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+    private void initFlashButtons(View view){
+        Button tapButton = view.findViewById(R.id.tap_button);
+        tapButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-              //  if (!isButtonLocked()) {
-                  //  setButtonLocked(true);
-                    tcpConnectSwitch.setClickable(false);
-                    if (isChecked) {
-                        if (!isTcpConnectionInProgres()) {
-                            (new Thread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    startTcpConnection();
-                                }
-                            })).start();
-                        } else {
-                            tcpConnectSwitch.setChecked(false);
-                            tcpConnectSwitch.setClickable(true);
+            public void onClick(View v) {
+                handleTouch();
+                if (bpmCalculator.times.size() >= 2){
+                    flashAtFixedRate(bpmCalculator.getDelay());
+                } else {
+                    executorService.execute(new Runnable() {
+                        @Override
+                        public void run() {
+                            pulseWhite();
                         }
-                    } else {
-                        if (isTcpConnectionInProgres()) {
-                            byte[] b = {-1, 0, 0, 0, 0};
-                            addToPacketQueue(b);
-                        }
+                    });
+                }
+            }
+        });
+
+        Button cancelButton = view.findViewById(R.id.cancel_flashing);
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cancelFlashing();
+            }
+        });
+
+        final Button flashBlue = view.findViewById(R.id.flash_blue);
+        flashBlue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                executorService.execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        pulseBlue();
                     }
-//                } else {
-//                    tcpConnectSwitch.setChecked(!isChecked);
-//                }
+                });
             }
         });
     }
 
-    private void startTcpConnection(){
-        sendUdpBroadCastPacket();
-        try{
-            ss = new ServerSocket(5558);
-            Socket client = ss.accept();
-            System.out.println("Accepted client...");
-            setTcpConnectionInProgres(true);
-            Button tcpConnectSwitch = getView().findViewById(R.id.tcpConnectionButton);
-            tcpConnectSwitch.setClickable(true);
+    private void initConnectDisconnectButtons(View view){
+        Button takeOverButton = view.findViewById(R.id.takeOverButton);
+        Button endConnectionButton = view.findViewById(R.id.endConnection);
 
-            OutputStream outputStream = client.getOutputStream();
-
-            while (isTcpConnectionInProgres()){
-                byte[] outByte = packetQueue.take().getPacket();
-                outputStream.write(outByte);
-                if (outByte[0] == TcpPacketContainer.END_PACKET){
-                    ss.close();
-
-
-                    setTcpConnectionInProgres(false);
-                    tcpConnectSwitch.setClickable(true);
-                }
-                System.out.println("Wrote packet...");
+        takeOverButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    executorService.execute(new Runnable() {
+                        @Override
+                        public void run() {
+                            establishDeviceIp();
+                        }
+                    });
             }
+        });
 
-        }catch(Exception e) {
-            Log.e(TAG,"Error reading queue", e);
-            setTcpConnectionInProgres(false);
-        }
-    }
-
-    private boolean addToPacketQueue(byte[] packet){
-        if (isTcpConnectionInProgres()){
-            try {
-                packetQueue.put(new TcpPacketContainer(packet));
-                return true;
-            } catch (InterruptedException e){
-                Log.e(TAG,"Error adding to queue", e);
+        endConnectionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                executorService.execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        closeConnection();
+                    }
+                });
             }
-        }
-        return false;
-    }
-
-    private void sendUdpBroadCastPacket(){
-        try {
-            InetAddress address = InetAddress.getByName("192.168.1.255");
-            DatagramSocket socket = new DatagramSocket();
-
-            byte[] buf = "rupertrgbtape".getBytes();
-            DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 5557);
-            socket.send(packet);
-        } catch (Exception e){
-            e.printStackTrace();
-        }
+        });
     }
 
     private void initShutdownRebootButtons(View view){
@@ -162,7 +217,7 @@ public class TcpDirectFragment extends Fragment {
                 @Override
                 public void onClick(View v) {
                     byte[] b ={TcpPacketContainer.SHUTDOWN_PACKET,0,0,0,0};
-                    addToPacketQueue(b);
+              //      addToPacketQueue(b);
                 }
             });
 
@@ -170,8 +225,155 @@ public class TcpDirectFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 byte[] b ={TcpPacketContainer.REBOOT_PACKET,0,0,0,0};
-                addToPacketQueue(b);
+              //  addToPacketQueue(b);
             }
         });
+    }
+
+    private String getBroadcastAddressLinux() throws BroadcastAddressNotFoundException {
+        try {
+            Process process = Runtime.getRuntime().exec(IF_CONFIG);
+            try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    String match = matchBroadcastAddress(line);
+                    if (match != null)
+                        return match;
+                }
+            }
+            throw new BroadcastAddressNotFoundException("No match");
+        } catch (IOException e){
+            throw new BroadcastAddressNotFoundException(e.getMessage());
+        }
+    }
+
+    private String matchBroadcastAddress(String line){
+        Pattern pattern = Pattern.compile("(broadcast )|(Bcast:)(\\d\\d?\\d?\\.){3}\\d\\d?\\d?");
+        Matcher matcher = pattern.matcher(line);
+        if (matcher.find()){
+            String withPrefix = matcher.group();
+
+            //Now just get IP
+            pattern = Pattern.compile("(\\d\\d?\\d?\\.){3}\\d\\d?\\d?");
+            matcher = pattern.matcher(withPrefix);
+            if (matcher.find())
+                return matcher.group();
+        }
+        return null;
+    }
+
+    private void handleTouch(){
+        bpmCalculator.recordTime();
+        restartResetTimer();
+        updateView();
+    }
+
+    private void restartResetTimer(){
+        stopResetTimer();
+        startResetTimer();
+    }
+
+    private void stopResetTimer() {
+        if (timer != null) {
+            timer.cancel();
+        }
+    }
+
+    private void startResetTimer() {
+        timer = new Timer("reset-bpm-calculator", true);
+        timer.schedule(new TimerTask() {
+
+            @Override
+            public void run() {
+                bpmCalculator.clearTimes();
+            }
+        }, RESET_DURATION);
+    }
+
+    private void updateView(){
+        Button b = getView().findViewById(R.id.tap_button);
+        if (bpmCalculator.times.size() >= 2){
+            int bpm = bpmCalculator.getBpm();
+            b.setText(Integer.valueOf(bpm).toString());
+        } else {
+            String tap = "tap";
+            b.setText(tap);
+        }
+    }
+
+    private void flashAtFixedRate(long milli){
+        if (scheduleTimer != null) {
+            scheduleTimer.cancel();
+        }
+        scheduleTimer = new Timer();
+        scheduleTimer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                pulseWhite();
+            }
+        }, 0, milli );
+    }
+
+    private void cancelFlashing(){
+        if (scheduleTimer != null){
+            scheduleTimer.cancel();
+        }
+    }
+
+    private void pulseBlue(){
+        try {
+            socket.send(onBlue);
+            socket.send(offFade);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private void pulseWhite(){
+        try {
+            socket.send(onWhite);
+            socket.send(offFade);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private void flashWhite(){
+        try {
+            socket.send(onWhite);
+            Thread.sleep(20);
+            socket.send(off);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private void initialisePackets(){
+        byte[] onWhite = new byte[8];
+        onWhite[1] = (byte) 255;
+        onWhite[2] = (byte) 255;
+        onWhite[3] = (byte) 255;
+
+        byte[] onBlue = new byte[8];
+        onBlue[3] = (byte) 255;
+
+        byte[] offFade = new byte[8];
+        System.arraycopy(ByteBuffer.allocate(4).putFloat(0.25f).array(),0,offFade,4,4);
+
+        byte[] offSnap = new byte[8];
+
+        customBrightness = new byte[8];
+
+        this.onWhite = new DatagramPacket(onWhite,onWhite.length,deviceIp,UDP_PORT);
+        this.onBlue = new DatagramPacket(onBlue,onWhite.length,deviceIp,UDP_PORT);
+        this.offFade = new DatagramPacket(offFade,onWhite.length,deviceIp,UDP_PORT);
+        this.off = new DatagramPacket(offSnap,onWhite.length,deviceIp,UDP_PORT);
+        this.customBrighnessPacket = new DatagramPacket(customBrightness,onBlue.length,deviceIp,UDP_PORT);
+    }
+
+    class BroadcastAddressNotFoundException extends IOException{
+        public BroadcastAddressNotFoundException(String message) {
+            super("Broadcast address could not be found; " + message);
+        }
     }
 }
