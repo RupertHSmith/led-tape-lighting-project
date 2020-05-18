179cb9d035ace5d840b53972722ebb6875b7fe3c
rhs1g18 <rhs1g18@soton.ac.uk>
2020-05-19 00:04:36 +0100

    added uart code class

---
+++ Project/src/common/UartCode.java
@@ -0,0 +1,23 @@
+package common;
+
+import java.io.IOException;
+
+class UartCode{
+	public native int getControlPanelIntensity();
+	public native void main();
+	
+	/*public static void main (String[] args){
+		UartCode uart = new UartCode();
+		uart.main();
+	}*/
+
+	static {
+		System.out.println("ATTEMPTING TO LOAD UART 00 02");
+		try {
+			NativeUtils.loadLibraryFromJar("/libUartCode.so");
+		} catch (IOException e){
+			e.printStackTrace();
+		}
+		System.out.println("LOADED UART");
+	}
+}
