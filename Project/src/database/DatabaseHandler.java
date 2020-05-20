8028424896fb06c61f102a57400aa47349ff12b0
rhs1g18 <rhs1g18@soton.ac.uk>
2020-05-20 15:58:12 +0100

    Added method to get database updater controller

--- Project/src/database/DatabaseHandler.java
+++ Project/src/database/DatabaseHandler.java
@@ -5,13 +5,18 @@ import common.Logger;
 import java.io.IOException;
 
 public class DatabaseHandler {
+    DeviceStateListener stateListener;
     public DatabaseHandler(DatabaseListener databaseListener, Logger logger) throws IOException {
-        DeviceStateListener stateListener = new DeviceStateListener(databaseListener, logger);
+        stateListener = new DeviceStateListener(databaseListener, logger);
 
 
         AlarmListener alarmListener = new AlarmListener(databaseListener, logger);
 
         new Thread(stateListener).start();
         new Thread(alarmListener).start();
+    }
+
+    public IUpdateDatabase getUpdateDatabaseController(){
+        return this.stateListener;
     }
 }
