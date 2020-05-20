594ddd19015bd4248c5708587bada81c50760736
rhs1g18 <rhs1g18@soton.ac.uk>
2020-05-20 18:51:19 +0100

    Implemented set intensity for StaticEffect

--- Project/src/effects/StaticEffect.java
+++ Project/src/effects/StaticEffect.java
@@ -8,8 +8,10 @@ public class StaticEffect implements IEffect, Runnable {
     private ITapeControl tapeControl;
     private int transition;
     private LedState colour;
+    private LedState unalteredColour;
     private int intensity;
     private Logger logger;
+    private boolean snapIntensityChange = false;
 
     private boolean terminated;
 
@@ -18,11 +20,6 @@ public class StaticEffect implements IEffect, Runnable {
 
     public StaticEffect(ITapeControl tapeControl, ArrayList<Long> colour, int intensity, int transition, Logger logger) throws InvalidTransitionTimeException {
         this(tapeControl, new LedState(colour.get(0), colour.get(1), colour.get(2)), intensity, transition, logger);
-    }
-
-    @Override
-    public void setIntensity(int intensity, boolean snap) {
-
     }
 
     public StaticEffect(ITapeControl tapeControl, LedState staticColour, int intensity, int transition, Logger logger) throws InvalidTransitionTimeException{
@@ -34,10 +31,18 @@ public class StaticEffect implements IEffect, Runnable {
         this.transition = transition;
         this.intensity = intensity;
         this.colour = LedState.applyIntensity(staticColour, intensity);
+        this.unalteredColour = staticColour;
         init();
+    }
 
-
-
+    @Override
+    public void setIntensity(int intensity, boolean snap) {
+        if (this.intensity != intensity) {
+            this.intensity = intensity;
+            tapeControl.haltRetainControl();
+            this.snapIntensityChange = snap;
+            this.colour = LedState.applyIntensity(unalteredColour, intensity);
+        }
     }
 
     public int getIntensity(){return this.intensity;}
@@ -83,6 +88,17 @@ public class StaticEffect implements IEffect, Runnable {
         //Run transition
         try {
             tapeControl.smartFade(colour, this);
+            //check for intensity change..
+            while (!terminated) {
+                if (!colour.equals(tapeControl.getColour())){
+                    if (snapIntensityChange){
+                        tapeControl.fadeTo(colour, 0.06f,this);
+                    } else {
+                        tapeControl.smartFade(colour, this);
+                    }
+                }
+
+            }
         } catch (TapeInUseException e){
             logger.writeError(this,e);
         }
